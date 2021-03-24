f6fb6c24128deff3f582579bbda41fa8fb281764
Jack Cross <jmc8252@psu.edu>
2021-03-24 12:34:41 -0400

    created comment cell file and linked to comment

---
+++ Parstagram/CommentCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentCell.swift
+//  Parstagram
+//
+//  Created by Jack Cross on 3/24/21.
+//
+
+import UIKit
+
+class CommentCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
